526f67e67357b6a423a80449ed01cfb7eac8e4eb
Arief Akhmad Fauzi <sekolah.arief@gmail.com>
2025-01-17 17:29:55 +0700

    fix

--- src/layouts/App.vue
+++ src/layouts/App.vue
@@ -1,11 +1,11 @@
 <template>
   <main
-    class="h-screen overflow-y-scroll w-full bg-dark-cover min-w-screen relative mx-auto pb-[84px]"
+    class="max-w-[900px] h-screen overflow-y-scroll w-full bg-dark-cover min-w-screen relative mx-auto pb-[84px]"
   >
     <slot />
   </main>
   <footer
-    class="fixed w-[90%] h-[74px] flex gap-2 justify-center items-center bottom-6 left-1/2 transform -translate-x-1/2 bg-dark-cover bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-lg rounded-full"
+    class="fixed max-w-[900px] w-[90%] h-[74px] flex gap-2 justify-center items-center bottom-6 left-1/2 transform -translate-x-1/2 bg-dark-cover bg-opacity-50 backdrop-filter backdrop-blur-lg shadow-lg rounded-full"
   >
     <div class="bg-dark w-16 h-16 flex justify-center items-center text-sm rounded-full shadow text-primary ">
         <box-icon class="fill-primary" name='user' ></box-icon>
